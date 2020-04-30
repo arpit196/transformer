3a7b0e7637918d9c386f187f02c97d104986eb39
arpit196 <30366755+arpit196@users.noreply.github.com>
2020-04-30 18:17:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -44,7 +44,7 @@ train_init_op = iter.make_initializer(train_batches)
 eval_init_op = iter.make_initializer(eval_batches)
 
 logging.info("# Load model")
-m = UniversalTransformer(hp)
+m = UniversalTransformer(hp, True)
 loss, train_op, global_step, train_summaries = m.train(xs, ys)
 y_hat, eval_summaries = m.eval(xs, ys)
 # y_hat = m.infer(xs, ys)
